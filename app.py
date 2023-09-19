fd2ee7fdb71203d822d909df0c3286b067b87a13
KonanMoise <bjrmoise06@icloud.com>
2023-09-19 21:14:49 +0000

    chargeDat dans app

--- app.py
+++ app.py
@@ -20,6 +20,36 @@ engine = create_engine('sqlite:///mokobank.db',
 Base.metadata.bind = engine
 db = scoped_session(sessionmaker(bind=engine))
 
+# charge donnees
+nom_utilisateur = 'C00000001'
+nom = 'Moise'
+user_type = 'gestionnaire'
+mdp = 'Moiseo@001'
+mdp_hash = bcrypt.generate_password_hash(mdp).decode('utf-8')
+db.execute(text("INSERT INTO utilisateurs (id,nom,user_type,mot_de_passe) VALUES (:u,:n,:t,:p)"),
+            {"u": nom_utilisateur,"n":nom,"t":user_type ,"p": mdp_hash})
+db.commit()
+print("compte Terminé ............................................ ")
+# user 2
+nom_utilisateur = 'C00000002'
+nom = 'Mental'
+user_type = 'caissier'
+mdp = 'Mental@002'
+mdp_hash = bcrypt.generate_password_hash(mdp).decode('utf-8')
+db.execute(text("INSERT INTO utilisateurs (id,nom,user_type,mot_de_passe) VALUES (:u,:n,:t,:p)"),
+            {"u": nom_utilisateur,"n":nom,"t":user_type ,"p": mdp_hash})
+db.commit()
+print("compte Terminé ............................................ ")
+# user 3
+nom_utilisateur = 'C00000003'
+nom = 'NaN'
+user_type = 'caissier'
+mdp = 'NaNDigit@003'
+mdp_hash = bcrypt.generate_password_hash(mdp).decode('utf-8')
+db.execute(text("INSERT INTO utilisateurs (id,nom,user_type,mot_de_passe) VALUES (:u,:n,:t,:p)"),
+            {"u": nom_utilisateur,"n":nom,"t":user_type ,"p": mdp_hash})
+db.commit()
+print("compte Terminé ............................................ ")
 
 # Code principal (Les Routes)
 @app.route('/')
